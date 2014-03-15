c71e2fc4c30bc1771f1df75302f73b713985f008
HTOch <HTOch@HTO_Envy>
2014-03-15 01:07:19 -0600

    #3 -> Creación Clase Torneo

---
+++ src/CPresentacion/Torneo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package CPresentacion;
+
+/**
+ *
+ * @author HTOch
+ */
+public class Torneo {
+    /*Atributos*/
+
+    /*Constructores*/
+
+    /*Gets*/
+
+    /*Sets*/
+
+    /*Metodos*/
+}
